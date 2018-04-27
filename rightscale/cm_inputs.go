ff87bfa6bf4b6256211ae5f9c21667ba7d79d7c4
Mark Dotson <mark+mastamark-github@rightscale.com>
2018-04-27 16:18:27 -0700

    STEEL-239 - clean up a bit

--- rightscale/cm_inputs.go
+++ rightscale/cm_inputs.go
@@ -13,22 +13,23 @@ import (
 // field.
 func cmInputs(d *schema.ResourceData) (rsc.Fields, error) {
 	var inputs rsc.Fields
-	var mapify map[string]string
-	mapify = make(map[string]string)
-	if f, ok := d.GetOk("inputs"); ok {
-		inputsList := f.(*schema.Set).List()
-		for _, inputIF := range inputsList {
-			v, ok := inputIF.(string)
-			if !ok {
-				return inputs, fmt.Errorf("inputsList does not appear to be properly handled as a string: %v", ok)
-			}
-			z := strings.Split(v, "=")
-			if len(z) != 2 {
-				return inputs, fmt.Errorf("input format should be formatted as 'KEY=type:value', split on '=' of '%s' should result in array count of 2, got count of: %v", z, len(z))
-			}
-			mapify[z[0]] = z[1]
-		}
-		inputs = rsc.Fields{"inputs": mapify}
+	mapify := make(map[string]string)
+	f, ok := d.GetOk("inputs")
+	if !ok {
+		return inputs, nil
 	}
+	il := f.(*schema.Set).List()
+	for _, i := range il {
+		v, ok := i.(string)
+		if !ok {
+			return inputs, fmt.Errorf("inputsList does not appear to be properly handled as a string: %v", ok)
+		}
+		z := strings.Split(v, "=")
+		if len(z) != 2 {
+			return inputs, fmt.Errorf("input format should be formatted as 'KEY=type:value', split on '=' of '%s' should result in array count of 2, got count of: %v", z, len(z))
+		}
+		mapify[z[0]] = z[1]
+	}
+	inputs = rsc.Fields{"inputs": mapify}
 	return inputs, nil
 }
